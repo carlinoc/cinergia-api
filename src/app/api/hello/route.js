a5270426b6c8328a18578a95497e996344d94527
CarlosPC <carlinoc@gmail.com>
2023-12-18 16:28:52 -0500

    Update env file and mysql file

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
-import {conn} from '@/libs/mysql'
 
 export async function GET() {
-    const result = await conn.query('select now()')
-    console.log(result)
-    return NextResponse.json({message: "Hello World"});
+    return NextResponse.json({message: "Hello World1"});
 }
--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -10,6 +10,6 @@ const globalForPrisma = global;
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+globalForPrisma.prisma = prisma;
 
 export default prisma;
